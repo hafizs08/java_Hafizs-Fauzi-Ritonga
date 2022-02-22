13a67db764789406d8cf55e2183766e7b9af2b52
hafizsritonga08@gmail.com <94980822+hafizs08@users.noreply.github.com>
2022-02-22 19:57:04 +0700

    commit mergei

--- 2_Version Control and Branch Managent (Git)/pratikum/coba.java
+++ 2_Version Control and Branch Managent (Git)/pratikum/coba.java
@@ -1,5 +1,6 @@
 class pertama{
  public static void main(String[] args){
-  System.out.println("Hello World lagi2");
+  System.out.println("Hello World stash2");
+  System.out.println("Hello World lagiasasa2aas");
  }   
 } 
