9932564e04f969318aacf6e7d3975cb233296fbd
hafizsritonga08@gmail.com <94980822+hafizs08@users.noreply.github.com>
2022-02-22 18:38:15 +0700

    commit lagi

--- 2_Version Control and Branch Managent (Git)/pratikum/coba.java
+++ 2_Version Control and Branch Managent (Git)/pratikum/coba.java
@@ -1,5 +1,9 @@
 class pertama{
  public static void main(String[] args){
+<<<<<<< Updated upstream
   System.out.println("Hello World stash");
+=======
+  System.out.println("Hello World lagiasasa2");
+>>>>>>> Stashed changes
  }   
 } 
