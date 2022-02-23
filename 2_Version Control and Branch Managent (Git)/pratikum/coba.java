3a81ae02991696cc0d29e76cb11487d8a28bbeed
hafizsritonga08@gmail.com <94980822+hafizs08@users.noreply.github.com>
2022-02-23 19:52:18 +0700

    tes

--- 2_Version Control and Branch Managent (Git)/pratikum/coba.java
+++ 2_Version Control and Branch Managent (Git)/pratikum/coba.java
@@ -1,5 +1,8 @@
 class pertama{
  public static void main(String[] args){
-  System.out.println("Hello Worldagi");
+  System.out.println("Hello World stash2");
+  System.out.println("Hello World lagiasasa2aasa");
+  System.out.println("Hello World 3");
+  System.out.println("Hello World lagiasasa2aas");
  }   
 } 
