a6ad7b073f54e875b568c3d571714b3058605233
hafizsritonga08@gmail.com <94980822+hafizs08@users.noreply.github.com>
2022-02-25 17:08:10 +0700

    again

--- 4_Basis Programming/pratikum/task2/src/com/company/Main.java
+++ 4_Basis Programming/pratikum/task2/src/com/company/Main.java
@@ -1,8 +1,23 @@
 package com.company;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner myObj = new Scanner(System.in);
+        String userName;
+
+        // Enter username and press Enter
+        System.out.println("Enter username");
+        userName = myObj.nextLine();
+
+        int o = 0;
+        int x = 0;
+
+        if (x == o) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+        System.out.println("Username is: " + userName);
     }
 }
--- 4_Basis Programming/pratikum/task2/src/task3.java
+++ 4_Basis Programming/pratikum/task2/src/task3.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
 public class task3 {
+    public static void main(String[] args)
+    {
+        int x;
+        Scanner input = new Scanner(System.in);
+        x = input.nextInt();
+        for(int i=1;i<=x;i++)
+        {
+            if(x%i == 0)
+            {
+                System.out.print(i + " ");
+            }
+        }
+    }
 }
