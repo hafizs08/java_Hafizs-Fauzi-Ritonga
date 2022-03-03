122e685beede751c45f8a1a0c5f6f7a32a82ef34
hafizsritonga08@gmail.com <94980822+hafizs08@users.noreply.github.com>
2022-03-04 01:44:19 +0700

    again

--- 8-Recursive, Number Theory, Sorting, Searching/pratikum/section8.java
+++ 8-Recursive, Number Theory, Sorting, Searching/pratikum/section8.java
@@ -1,21 +1,23 @@
+import java.util.Scanner;
 public class section8 {
-     public static void main(String[] args) {
-        int  n=20;
-        System.out.println("All Prime Numbers Between 1 to "+n);
-        boolean isPrime;
-        
-        for (int i = 2; i <= n; i++) {
-            isPrime = true;
-            for (int j = 2; j <=Math.sqrt(i); j++) {
-                if (i % j == 0) {
-                    isPrime = false;
-                    break;
-                }
-            }
-            if (isPrime)
-                System.out.print(i + " ");
-                
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int isi=input.nextInt();
+        long[] prima = new long[isi];
+        prima[0] = 2;
+        prima[1]=3;
+        int hitung = 2;
+        long angka = 5;
+        luar:
+        for (; hitung < prima.length; angka +=2){
+            long limit = (long)Math.ceil(Math.sqrt((double) angka));
+            for (int i = 1; i < hitung && prima[i] <=limit; i++)
+                if (angka%prima[i] == 0)
+                    continue luar;        
+               prima[hitung++] = angka;
+           
         }
-     }
- 
+            System.out.println(prima[hitung-1]);
+    }
+    
  }
