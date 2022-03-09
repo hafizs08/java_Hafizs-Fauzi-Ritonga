2b3ffab86f2c1dffb5f4b86c302204c139425872
hafizsritonga08@gmail.com <94980822+hafizs08@users.noreply.github.com>
2022-03-09 17:40:53 +0700

    again

--- 9-Problem Solving Brute Force/pratikum/task2.java
+++ 9-Problem Solving Brute Force/pratikum/task2.java
@@ -1,9 +1,28 @@
-public class task2 {
-    public static void main (String args[]){
-        int n = 11;
-    }
-    static void task2(int n){
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
+public class task2 {
+    public static void main (String [] args){
+        System.out.println(task2(123));
+    }
+    static List<Integer> task2(int M){
+        List<Integer> mc = Arrays.asList(1,10,20,50,100,200,500,1000,2000,5000);
+        Collections.sort(mc, Collections.reverseOrder());
+
+        List<Integer> hsl = new ArrayList<>();
+        while(M>0){
+            for(int i=0;i<mc.size();i++){
+                if(M>=mc.get(i)){
+                    hsl.add(mc.get(i));
+                    M-=mc.get(i);
+                    break;
+                }
+            }
+        }
+        return hsl;
     }
     
 }
---
+++ 9-Problem Solving Brute Force/summry.md
@@ -0,0 +1,6 @@
+Algoritma Brute force adalah sebuah pendekatan yang mudah untuk memecahkan suatu masalah, biasanya didasarkan langsung pada pernyataan masalah dan definisi konsep yang dilibatkan. Algoritma ini memecahkan masalah dengan sangat sederhana, langsung, dan dengan cara yang cukup jelas.
+
+Algoritma adalah langkah dalam mencari solusi atas sebuah masalah. banyak sekali algoritma yang dapat kita gunakan dalam membangun sebuah program , salah satunya adalah algoritma greedy.
+Algoritma greedy merupakan metode yang paling populer untuk memecahkan persoalan optimasi.
+
+Algoritma Divide and Conquer adalah strategi pemecahan masalah yang besar dengan cara melakukan pembagian masalah yang besar tersebut menjadi beberapa bagian yang lebih kecil secara rekursif hingga masalah tersebut dapat dipecahkan secara langsung.
