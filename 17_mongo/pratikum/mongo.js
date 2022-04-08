5cb71e56cb2fd744c15c88e795de8c3e73b8b126
hafizsritonga08@gmail.com <94980822+hafizs08@users.noreply.github.com>
2022-04-09 04:32:20 +0700

    again

--- 17_mongo/pratikum/mongo.js
+++ 17_mongo/pratikum/mongo.js
@@ -1,1 +1,48 @@
-mongo.js
+db.books.find({authorID:1,authorID:2})
+db.books.find({authorID:1}, {
+    _id: 1,
+    title: 1 
+});
+
+
+db.books.aggregate([{$group: {_id:"authorID:2", sum_val:{$sum:"$stats.page"}}}])
+
+
+db.collection.aggregate([
+    { "$addFields": { "custom_field": "$obj.obj_field1" } }
+])
+db.books.aggregate( [
+    { $project: { _id: 1 } },
+    { $merge : { into : "authors" } }
+ ] )
+
+ db.books.aggregate( [
+    {
+      $lookup:
+        {
+          from: "authors",
+          localField: "authorID",
+          foreignField: "_id",
+          as: "inventory_docs"
+        }
+   }
+ ] )
+ db.authors.aggregate( [
+    {
+      $lookup:
+        {
+          from: "books",
+          localField: "_id",
+          foreignField: "authorID",
+          as: "inventory_docs"
+        }
+   },
+    {
+        $lookup: {
+            from: "publishers",
+            localField: "_id",
+            foreignField: "publisheriD",
+            as: "publisher"
+        }
+    }
+ ] )
