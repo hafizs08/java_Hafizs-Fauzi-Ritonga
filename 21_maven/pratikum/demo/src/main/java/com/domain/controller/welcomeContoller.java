7acadfb94df214bc1f821164cd34cb79a2a36b0f
hafizsritonga08@gmail.com <94980822+hafizs08@users.noreply.github.com>
2022-04-14 23:06:52 +0700

    again

--- 21_maven/pratikum/demo/src/main/java/com/domain/controller/welcomeContoller.java
+++ 21_maven/pratikum/demo/src/main/java/com/domain/controller/welcomeContoller.java
@@ -1,16 +1,28 @@
 package com.domain.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/v1/message")
 public class welcomeContoller {
-  
-
         @GetMapping
-        public String welcome() {
+        public String get() {
+            return "Welcome to Spring Boot";
+        
+
+    }
+    @PostMapping
+        public String post() {
+            return "Welcome to Spring Boot";
+        
+
+    }
+    @DeleteMapping
+        public String delete() {
             return "Welcome to Spring Boot";
         
 
